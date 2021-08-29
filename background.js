8e33ba6e3263bf3deab84b4b64bd526174b94721
jenny270ren <sijin.ren21@ycdsbk12.ca>
2021-08-28 22:20:56 -0400

    “change”

--- background.js
+++ background.js
@@ -1,60 +1,43 @@
 
-// default
-  // chrome.runtime.onInstalled.addListener(() => {
-  //   // opens a tab with information
-  //   chrome.tabs.create({url:'\options.html'},function(){})
-  // });
 function windowClear() {
   window.localStorage.clear();
   window.localStorage.removeItem(1);
   window.localStorage.removeItem("title");
 }
-//windowClear();
-    
-//button to clear ? mb works???
+
+//button to clear local storage and close extension
 let clearAllButton = document.getElementById('clearAll');
-clearAllButton.addEventListener('click', function(){
+clearAllButton.addEventListener('click', function () {
   windowClear();
+  window.close();
 })
 
-
 if ("title" in localStorage) {
-    alert("length of local storage 'title': " + JSON.parse(window.localStorage.getItem("title")).length);
-    for (let i = 0; i < JSON.parse(window.localStorage.getItem("title")).length; i++){
-      addToList(i);
-      alert("for loop repetitions:" + i);
-    }
-} 
-
+  for (let i = 0; i < JSON.parse(window.localStorage.getItem("title")).length; i++) {
+    addToList(i);
+  }
+}
 
 let button = document.getElementById('saveTabs');
-  //button.addEventListener("click", getTabs);
-  
 let windowsData;
+//an array containing an object of all the tab titles and their corresponding urls  
 let allWindowsTabs = [];
-  //an array containing an object of all the tab titles and their corresponding urls  
 
-function logTabsForWindows(windowInfo) { //windowInfoArray is the array of window objects 
-  
-    if (window.localStorage.getItem(1) === null){
-      alert("if1");
-      allWindowsTabs.push(windowInfo);      
-    }
-    else if (allWindowsTabs.length === 0){
-      alert("if2");
-      allWindowsTabs = JSON.parse(window.localStorage.getItem(1));
-      alert(allWindowsTabs);
+//windowInfoArray is the array of window objects 
+function logTabsForWindows(windowInfo) {
+  if (window.localStorage.getItem(1) === null) {
+    allWindowsTabs.push(windowInfo);
+  }
+  else if (allWindowsTabs.length === 0) {
+    allWindowsTabs = JSON.parse(window.localStorage.getItem(1));
+    allWindowsTabs.push(windowInfo);
+  }
+  else {
+    allWindowsTabs.push(windowInfo);
+  }
 
-      allWindowsTabs.push(windowInfo);
-      alert(allWindowsTabs);
-    }
-    else{
-      alert("if3");
-      allWindowsTabs.push(windowInfo);
-      
-    }
-
-    window.localStorage.setItem(1, JSON.stringify(allWindowsTabs)); //one key value pair but all the things are stored in the value
+  //one key value pair but all the things are stored in the value
+  window.localStorage.setItem(1, JSON.stringify(allWindowsTabs));
 }
 
 function onError(error) {
@@ -66,89 +49,57 @@ function getTabs() {
     populate: true,
     windowTypes: ["normal"]
   });
+
   getting.then(logTabsForWindows, onError);
 }
 
+button.addEventListener('click', function () {
+  getTabs();
+  const inputbox = document.getElementById('create-name');
 
-
-  
-
-  // inputName = null
-button.addEventListener('click', function(){
-    const inputbox = document.getElementById('create-name');
-      if (inputbox.style.display == "none") {
-        inputbox.style.display = "block";
-    } else {
-        inputbox.style.display = "none";
-    }
+  if (inputbox.style.display == "none") {
+    inputbox.style.display = "block";
+  } else {
+    inputbox.style.display = "none";
+  }
 })
 
- 
-  //----------------------------------------------------------------------
+//----------------------------------------------------------------------
 let submitbutton = document.getElementById('submit');
-let inputName=[];
+let inputName = [];
 
-submitbutton.addEventListener('click', function(){
-    getTabs();  
-    let input = document.getElementById("current").value;
-      
-    if (window.localStorage.getItem("title") === null){
-      // alert("if1");
-        inputName = [input];
-        
-    }
-    else if (inputName.length === 0){
-      // alert("if2");
-        
-      inputName = JSON.parse(window.localStorage.getItem("title"));
-        // alert(inputName);
-        // alert(typeof(inputName));
-        
-      inputName.push(input);
-        // alert(inputName);
-    }        
-    else{
-        // alert("if3");
-        // alert(inputName);
-      inputName.push(input);
-    }
+submitbutton.addEventListener('click', function () {
+  let input = document.getElementById("current").value;
 
-    window.localStorage.setItem("title", JSON.stringify(inputName));
-      //window.localStorage.setItem("title", inputName);
+  if (window.localStorage.getItem("title") === null) {
+    inputName = [input];
+  }
+  else if (inputName.length === 0) {
+    inputName = JSON.parse(window.localStorage.getItem("title"));
+    inputName.push(input);
+  }
+  else {
+    inputName.push(input);
+  }
 
-    addToList(JSON.parse(window.localStorage.getItem("title")).length-1);
-      //addToList(window.localStorage.getItem("title").length-1);
+  window.localStorage.setItem("title", JSON.stringify(inputName));
 
- })
-  //----------------------------------------------------------------------
+  addToList(JSON.parse(window.localStorage.getItem("title")).length - 1);
+})
+//----------------------------------------------------------------------
 
-function addToList(x){
-    
+function addToList(x) {
+  var list1 = document.getElementById('workspace-list');
+  var workspaceName = JSON.parse(window.localStorage.getItem("title"))[x]
+  var workspace = workspaceName + "1";
+  var urlList = workspaceName + "2";
 
-    var list1 = document.getElementById('workspace-list');
-    // var entry = document.createElement("li");
-    // entry.appendChild(document.createTextNode(inputName));
-    // list1.appendChild(entry);
-    var workspaceName = JSON.parse(window.localStorage.getItem("title"))[x]
-    var workspace = workspaceName+ "1";
-    var urlList = workspaceName +"2";
-    document.getElementById('workspace-list').innerHTML += '<li id ='+ workspace +'><a href="#">'+workspaceName+'</a></li>'; //works!
-    document.getElementById(workspace).innerHTML += '<ul id = '+ urlList +'>' +'</ul>';
+  document.getElementById('workspace-list').innerHTML += '<li id =' + workspace + '><a href="#">' + workspaceName + '</a></li>'; //works!
+  document.getElementById(workspace).innerHTML += '<ul id = ' + urlList + '>' + '</ul>';
 
+  let window1tabs = JSON.parse(window.localStorage.getItem(1))[x].tabs;
 
-    
-    let window1tabs = JSON.parse(window.localStorage.getItem(1))[x].tabs;
-    
-
-
-    for (let i = 0; i < window1tabs.length; i++){
-      document.getElementById(urlList).innerHTML += '<li><a href="'+ window1tabs[i].url +'" target = "_blank">' + window1tabs[i].title +'</a></li>';
-    }
-
+  for (let i = 0; i < window1tabs.length; i++) {
+    document.getElementById(urlList).innerHTML += '<li><a href="' + window1tabs[i].url + '" target = "_blank">' + window1tabs[i].title + '</a></li>';
+  }
 }
- 
-  
-  
-  
-  
-  
